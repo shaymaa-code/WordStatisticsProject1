d5304a585fcc35f56f6a5ed4305b23e45c5712ed
inveaii <thisisfatimasemail@gmail.com>
2025-12-06 19:31:06 +0200

    gui in Mac
    done

--- WordStatisticsProject/src/wordstatisticsproject/WordStatisticsProject.java
+++ WordStatisticsProject/src/wordstatisticsproject/WordStatisticsProject.java
@@ -10,11 +10,28 @@ package wordstatisticsproject;
  */
 import gui.MainWindow;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager; //for buttons to show their color on Mac
 
 public class WordStatisticsProject {
+
     public static void main(String[] args) {
-        // Launch the GUI
+
+        // --- 1. SET THEME TO NIMBUS (Fixes Purple Buttons) ---
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println("Nimbus theme not available. Using default.");
+        }
+        // -----------------------------------------------------
+
+        // --- 2. LAUNCH THE GUI ---
         SwingUtilities.invokeLater(() -> {
+            // Your GUI class is named 'MainWindow', so we use that here
             MainWindow window = new MainWindow();
             window.setVisible(true);
         });
